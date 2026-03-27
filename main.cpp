c7bb90f9c669a3ddfafc9a7404af735d1ffe14af
Peter Dimov <pdimov@gmail.com>
2026-03-27 22:02:13 +0200

    Add benchmark using simple socket source/sink

--- main.cpp
+++ main.cpp
@@ -2,38 +2,45 @@
 // Distributed under the Boost Software License, Version 1.0.
 // https://www.boost.org/LICENSE_1_0.txt
 
+#include "proto_read.hpp"
+#include "proto_write.hpp"
+#include "simple_socket_source.hpp"
+#include "simple_socket_sink.hpp"
+#include "reader_task.hpp"
+#include "writer_task.hpp"
 #include <boost/corosio/io_context.hpp>
-#include <boost/corosio/tcp_socket.hpp>
 #include <boost/corosio/test/socket_pair.hpp>
 #include <boost/capy/ex/run_async.hpp>
-#include <boost/capy/task.hpp>
-#include <boost/capy/read.hpp>
-#include <boost/capy/write.hpp>
-#include <boost/capy/buffers.hpp>
+#include <boost/current_function.hpp>
+#include <chrono>
+#include <cstdio>
 
 namespace corosio = boost::corosio;
 namespace capy = boost::capy;
 
-capy::task<void> writer_task( corosio::tcp_socket sock )
-{
-    unsigned char const data[ 4 ] = {};
-    co_await capy::write( sock, capy::const_buffer{ data, sizeof(data) } );
-}
-
-capy::task<void> reader_task( corosio::tcp_socket sock )
-{
-    unsigned char data[ 4 ] = {};
-    co_await capy::read( sock, capy::mutable_buffer{ data, sizeof(data) } );
-}
-
-int main()
+template<class Source, class Sink> void bench()
 {
     corosio::io_context ioc;
 
     auto [rs, ws] = corosio::test::make_socket_pair( ioc );
 
-    capy::run_async( ioc.get_executor() )( writer_task( std::move(ws) ) );
-    capy::run_async( ioc.get_executor() )( reader_task( std::move(rs) ) );
+    std::printf( "-- %s:\n", BOOST_CURRENT_FUNCTION );
+
+    auto t1 = std::chrono::steady_clock::now();
+
+    capy::run_async( ioc.get_executor() )( writer_task( Sink( std::move(ws) ) ) );
+    capy::run_async( ioc.get_executor() )( reader_task( Source( std::move(rs) ) ) );
 
     ioc.run();
+
+    auto t2 = std::chrono::steady_clock::now();
+
+    using namespace std::chrono_literals;
+
+    std::printf( "-- %lld ms\n\n", static_cast<long long>( ( t2 - t1 ) / 1ms ) );
+}
+
+int main()
+{
+    bench<simple_socket_source, simple_socket_sink>();
 }
