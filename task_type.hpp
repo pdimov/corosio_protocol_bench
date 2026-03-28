ca372fb51f5d1adc7a1ae5f5e1ed20ee10102c5d
Peter Dimov <pdimov@gmail.com>
2026-03-28 04:15:44 +0200

    Add task_type.hpp, socket_type.hpp

---
+++ task_type.hpp
@@ -0,0 +1,12 @@
+#ifndef TASK_TYPE_HPP_INCLUDED
+#define TASK_TYPE_HPP_INCLUDED
+
+// Copyright 2026 Peter Dimov
+// Distributed under the Boost Software License, Version 1.0.
+// https://www.boost.org/LICENSE_1_0.txt
+
+#include <boost/capy/task.hpp>
+
+using task_type = boost::capy::task<void>;
+
+#endif // #ifndef TASK_TYPE_HPP_INCLUDED
